63fa530565fa48e5fe7d3fa83eeb3dc9989d9a58
mkor92 <mkorhonen92@gmail.com>
2022-11-16 13:48:07 +0100

    HTML,SASS and js for working pop-up modal

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const filterSection = document.querySelector(".filter");
 const filterButton = document.querySelector("#filterButton");
-const filterCloseButton = document.querySelector(".filterCloseButton")
+const filterCloseButton = document.querySelector(".filterCloseButton");
 
 document.querySelector(".main-nav-toggle").addEventListener("click", () => {
   document.querySelector(".main-nav").classList.toggle("open");
@@ -8,12 +8,33 @@ document.querySelector(".main-nav-toggle").addEventListener("click", () => {
 
 filterButton.addEventListener("click", () => {
   filterSection.style.display = "flex";
-  filterButton.style.display = "none"
+  filterButton.style.display = "none";
   filterButton.setAttribute("aria-expanded", true);
-})
+});
 
 filterCloseButton.addEventListener("click", () => {
   filterSection.style.display = "none";
-  filterButton.style.display = "block"
+  filterButton.style.display = "block";
   filterButton.setAttribute("aria-expanded", false);
-})
+});
+
+// Modal
+const openModal = document.querySelector("#modal-open");
+const modal = document.querySelector(".modal");
+const closeModal = document.querySelector(".modal-close");
+
+// When user clicks on "book this room", open modal
+openModal.addEventListener("click", function () {
+  modal.style.display = "block";
+});
+// When user clicks on "X", close modal
+closeModal.addEventListener("click", function () {
+  modal.style.display = "none";
+});
+
+// If user clicks outside of modal pop-up, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
