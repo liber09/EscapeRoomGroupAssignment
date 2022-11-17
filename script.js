b3973f2c5507a2215f5153a9358305b0fa81bb50
mkor92 <mkorhonen92@gmail.com>
2022-11-17 19:37:43 +0100

    Modal window 1 created true JS and CSS

--- script.js
+++ script.js
@@ -1,44 +1,99 @@
-const filterSection = document.querySelector(".filter");
-const filterButton = document.querySelector("#filterButton");
-const filterCloseButton = document.querySelector(".filterCloseButton");
-
+// -------------------- MOBILE MENU --------------------
 document.querySelector(".main-nav-toggle").addEventListener("click", () => {
   document.querySelector(".main-nav").classList.toggle("open");
 });
 
-filterButton.addEventListener("click", () => {
-  filterSection.style.display = "flex";
-  filterButton.style.display = "none";
-  filterButton.setAttribute("aria-expanded", true);
-});
+// -------------------- FILTER SECTION  --------------------
+// Only runs filter code if it is on the right html file
+const host = "http://127.0.0.1:5500/";
+if (window.location.href == host + "challenges.html") {
+  const filterSection = document.querySelector(".filter");
+  const filterButton = document.querySelector("#filterButton");
+  const filterCloseButton = document.querySelector(".filterCloseButton");
 
-filterCloseButton.addEventListener("click", () => {
-  filterSection.style.display = "none";
-  filterButton.style.display = "block";
-  filterButton.setAttribute("aria-expanded", false);
-});
+  filterButton.addEventListener("click", () => {
+    filterSection.style.display = "flex";
+    filterButton.style.display = "none";
+    filterButton.setAttribute("aria-expanded", true);
+  });
+
+  filterCloseButton.addEventListener("click", () => {
+    filterSection.style.display = "none";
+    filterButton.style.display = "block";
+    filterButton.setAttribute("aria-expanded", false);
+  });
+}
 
 // -------------------- MODAL --------------------
-const openModal = document.querySelectorAll(".modal-open");
-const modal = document.querySelector(".modal");
-const closeModal = document.querySelector(".modal-close");
 
-// When user clicks on "book this room", open modal
+// Trigger "book this room" to open modal
+const openModal = document.querySelectorAll(".modal-open");
+
+// Backdrop when modal is open
+const backDrop = document.createElement("section");
+backDrop.classList.add("backdrop");
+
+// Creates the modal window
+const modal = document.createElement("div");
+modal.classList.add("modal-content");
+
+// Creates H3 element to use in modal
+const modalHeading = document.createElement("h3");
+modalHeading.classList.add("modal-heading");
+
+// Close button "X" in modal
+const closeBtn = document.createElement("small");
+closeBtn.classList.add("modal-close");
+closeBtn.innerHTML = "&times;";
+
+// Function to close modal if click outside of modalbox
+function closeModal() {
+  window.onclick = function (event) {
+    if (event.target == backDrop) {
+      backDrop.remove();
+    }
+  };
+}
+const inputDate = document.createElement("input");
+const labelDate = document.createElement("label");
+const question = document.createElement("p");
+// When user clicks on "book this room", run and create function to open modal
 openModal.forEach(function (e) {
   e.addEventListener("click", function () {
-    modal.style.display = "block";
+    document.body.append(backDrop);
+    backDrop.addEventListener("click", closeModal);
+
+    // Remove modal when click on X
+    modal.appendChild(closeBtn);
+    closeBtn.addEventListener("click", function () {
+      backDrop.remove();
+    });
+
+    // Set heading inside modal
+
+    modalHeading.innerHTML =
+      'Book room <span class="room-title">"Title of room"</span> (step 1)';
+    modal.appendChild(modalHeading);
+
+    backDrop.appendChild(modal);
+    // Question in modal
+
+    question.style.margin = "40px";
+    question.innerText = "What date would you like to come?";
+    modal.appendChild(question);
+
+    // Label for input
+
+    labelDate.setAttribute("for", "date");
+    labelDate.innerText = "Date:";
+    modal.appendChild(labelDate);
+
+    // Date input
+
+    inputDate.id = "date";
+    inputDate.type = "date";
+    modal.appendChild(inputDate);
   });
 });
-// When user clicks on "X", close modal
-closeModal.addEventListener("click", function () {
-  modal.style.display = "none";
-});
-
-// If user clicks outside of modal-content, close it
-window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-};
 
 // -------------------- END OF MODAL JS --------------------
