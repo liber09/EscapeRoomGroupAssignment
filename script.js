9665695beabbfb9ef40d5aa85f6d9ba7fffeb05a
Rasmus Eliasson <ras.muse@hotmail.com>
2022-11-18 22:09:15 +0100

    modal 2 and 3 needs to add styling and conditions

--- script.js
+++ script.js
@@ -100,3 +100,154 @@ openModal.forEach(function (e) {
 // -------------------- END OF MODAL JS --------------------
 
 //---------------------- CREATING MODAL2 ----------------------
+
+// --------------- Waiting for button to apply modalPopUp2 & 3------------
+// --------------------------- needs to apply remove the first modal------------------
+
+function modalPopUp2() {
+  //variables
+  const modalSection = document.createElement("section");
+  modalSection.classList.add("backdrop");
+  const modal = document.createElement("div");
+  modal.classList.add("modal-content");
+  const form = document.createElement("form");
+  form.classList.add("form-content");
+  const headingModal = document.createElement("h1");
+  headingModal.classList.add("heading-modal");
+  const confirmBtn = document.createElement("button");
+  confirmBtn.innerText = "Submit booking";
+  confirmBtn.classList.add("submit-booking");
+
+  //object and array for storing input value
+  const bookingInfo = {};
+  let completedBooking = []; //maybe use later
+
+  //errormessages if input is empty !!!!!! not able yet
+  const errorMessages = {
+    nameError: "You must enter your name!",
+    emailError: "You must enter an email!",
+    timeError: "You must enter a time you want to visit!",
+  };
+
+  //creating modal
+  document.body.append(modalSection);
+  modalSection.append(modal);
+
+  modal.append(form);
+  form.append(headingModal);
+  headingModal.innerText = 'Book room "Title of room" (step 2)';
+
+  const nameLabel = document.createElement("p");
+  nameLabel.classList.add("label");
+  nameLabel.innerText = "Name?";
+  const input1 = document.createElement("input");
+  input1.classList.add("input");
+  input1.type = "text";
+
+  const emailLabel = document.createElement("p");
+  emailLabel.classList.add("label");
+  emailLabel.innerText = "Email?";
+
+  const input2 = document.createElement("input");
+  input2.classList.add("input");
+  input2.type = "email";
+
+  const timeLabel = document.createElement("p");
+  timeLabel.classList.add("label");
+  timeLabel.innerText = "What time would you like to come?";
+  const input3 = document.createElement("input");
+  input3.classList.add("input");
+
+  const participantsLabel = document.createElement("p");
+  participantsLabel.classList.add("label");
+  participantsLabel.innerText = "How many?";
+  const input4 = document.createElement("select");
+  input4.classList.add("input");
+
+  const option2 = document.createElement("option");
+  option2.innerText = "2 participants";
+  const option3 = document.createElement("option");
+  option3.innerText = "3 participants";
+  const option4 = document.createElement("option");
+  option4.innerText = "4 participants";
+  const option5 = document.createElement("option");
+  option5.innerText = "5 participants";
+  const option6 = document.createElement("option");
+  option6.innerText = "6 participants";
+
+  const closeBtn2 = document.createElement("small");
+  closeBtn2.classList.add("modal-close");
+  closeBtn2.innerHTML = "&times;";
+
+  //appending all created elements to modal
+  form.append(
+    nameLabel,
+    input1,
+    emailLabel,
+    input2,
+    timeLabel,
+    input3,
+    participantsLabel,
+    input4,
+    confirmBtn,
+    closeBtn2
+  );
+
+  input4.append(option2, option3, option4, option5, option6);
+
+  //closing modal on X Icon
+  closeBtn2.addEventListener("click", function () {
+    modalSection.remove();
+  });
+
+  // storing user input in object an array when submit button is pressed.
+  confirmBtn.addEventListener("click", function () {
+    bookingInfo.nameInfo = input1.value;
+    bookingInfo.emailInfo = input2.value;
+    bookingInfo.timeInfo = input3.value;
+    bookingInfo.participantInfo = input4.value;
+
+    completedBooking.push(bookingInfo);
+
+    form.remove(); //removing the "form" element from modal 2
+    modalPopUp3(); // replacing vid modal 3
+  });
+
+  //------------------------- Modal 3 ------------------------------
+
+  function modalPopUp3() {
+    //creating elements
+    const confirmationHeading = document.createElement("h1");
+    confirmationHeading.classList.add("heading-modal");
+    confirmationHeading.innerText = "Your booking has been confirmed";
+
+    const bookingDoneText1 = document.createElement("h2");
+    const bookingDoneText2 = document.createElement("h2");
+    const bookingDoneText3 = document.createElement("h2");
+    const bookingDoneText4 = document.createElement("h2");
+
+    //displaying info about the booking
+    bookingDoneText1.innerText = "Your team name is: " + bookingInfo.nameInfo;
+    bookingDoneText2.innerText =
+      "We have sent a confirmitation mail to: " + bookingInfo.emailInfo;
+    bookingDoneText3.innerText = "Your time is: " + bookingInfo.timeInfo;
+    bookingDoneText4.innerText =
+      "This is room is booked for " + bookingInfo.participantInfo;
+
+    //return to homepage tag
+    const homePageBtn = document.createElement("a");
+    homePageBtn.setAttribute("href", "challenges.html");
+    homePageBtn.classList.add("submit-booking");
+    homePageBtn.innerText = "Return to homepage";
+
+    //appending all element to modal 3
+    modal.append(
+      confirmationHeading,
+      bookingDoneText1,
+      bookingDoneText2,
+      bookingDoneText3,
+      bookingDoneText4,
+      homePageBtn
+    );
+  }
+}
