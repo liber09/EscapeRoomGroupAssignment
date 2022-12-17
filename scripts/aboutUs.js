cdd02e243a14b70e1d83235f8ea0b319723a087e
mkor92 <mkorhonen92@gmail.com>
2022-12-17 17:17:48 +0100

    about us page completed, responsive design

---
+++ scripts/aboutUs.js
@@ -0,0 +1,4 @@
+// -------------------- MOBILE MENU --------------------
+document.querySelector(".main-nav-toggle").addEventListener("click", () => {
+  document.querySelector(".main-nav").classList.toggle("open");
+});
